018ee15cfb6e470e3eda8c4b882fee319e22fc25
ravi-2208 <udaragudiravikishore@gmail.com>
2025-11-06 20:30:17 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,15 @@
+//Client.js
+const http = require('http');
+
+http.get('http://localhost:3000', (res) => {
+    let data = '';
+
+    res.on('data', chunk => {
+        data += chunk;
+    });
+
+    res.on('end', () => {
+        console.log('Server says:', data);
+    });
+});
+
---
+++ constant.js
@@ -0,0 +1,6 @@
+//constant.js
+
+const content = 'HTML, CSS, Javascript, Typescript, MongoDB, Express.js, React.js, Node.js';
+// Create file and write content
+fs.writeFileSync('src.txt', content);
+console.log('src.txt created successfully!');
